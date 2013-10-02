01f71acca035f05074fc459382fb41e455e1329b
liyicky <liyicky@gmail.com>
2013-10-02 10:38:13 -0400

    Forgot to actually add the proj, heh..

---
+++ ExampleProj/liygem_example.rb
@@ -0,0 +1,14 @@
+require 'liygem'
+
+class BarJoke
+    def self.badJoke
+        puts "An White guy, a Spanish guy, a Chinese guy, and A Ukrainian guy walk into a world."
+        Hi.sayHello()
+        Hi.sayHello("spanish")
+        Hi.sayHello("chinese")
+        Hi.sayHello("ukrainian")
+
+    end
+end
+
+BarJoke.badJoke
