bddca80b9f6cdeee366983d9f3d87b94b0b75818
liyicky <liyicky@gmail.com>
2013-10-05 13:54:23 -0400

    Modified the example proj to use namespacing

--- ExampleProj/liygem_example.rb
+++ ExampleProj/liygem_example.rb
@@ -3,10 +3,10 @@ require 'liygem'
 class BarJoke
     def self.badJoke
         puts "An White guy, a Spanish guy, a Chinese guy, and A Ukrainian guy walk into a world."
-        Hi.sayHello()
-        Hi.sayHello("spanish")
-        Hi.sayHello("chinese")
-        Hi.sayHello("ukrainian")
+        puts Liygem::sayHello()
+        puts Liygem::sayHello("spanish")
+        puts Liygem::sayHello("chinese")
+        puts Liygem::sayHello("ukrainian")
 
     end
 end
